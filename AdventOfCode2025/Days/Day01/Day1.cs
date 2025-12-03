286ba87ae978dfe894439fd140119e170bf619cc
Graham Schomp <39341404+SCHOOMP@users.noreply.github.com>
2025-12-02 19:43:53 -0600

    Initial comit - Advent of Code is here!!!!

---
+++ AdventOfCode2025/Days/Day01/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2025.Days;
+
+public static class Day01
+{
+    public static void Run()
+    {
+        string inputPath = Path.Combine("Days", "Day01", "input.txt");
+        string[] input = File.ReadAllLines(inputPath);
+        int dial = 50;
+        int zeroIncrement = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string current = input[i];
+            char direction = current[0];
+            string distancePart = current.Substring(1);
+            int distance = int.Parse(distancePart);
+
+            if (direction == 'R')
+            {
+                dial = (dial + distance) % 100;
+                zeroIncrement = CheckIfZero(dial, zeroIncrement);
+            }
+
+            if (direction == 'L')
+            {
+                dial = (dial - distance + 100) % 100; // ensure positive modulo
+                zeroIncrement = CheckIfZero(dial, zeroIncrement);
+            }
+        }
+
+        Console.WriteLine("Password is " + zeroIncrement);
+
+
+    }
+
+    private static int CheckIfZero(int dial, int increment)
+    {
+        if (dial == 0)
+        {
+            increment += 1;
+        }
+        
+        return increment;
+    }
+}
---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AdventOfCode2025.Days;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter day (1-25): ");
+        int day = int.Parse(Console.ReadLine()!);
+
+        switch (day)
+        {
+            case 1: Day01.Run(); break;
+           
+            // ...
+            // case 25: Day25.Run(); break;
+            default: Console.WriteLine("Invalid day"); break;
+        }
+    }
+}
